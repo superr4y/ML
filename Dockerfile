03040bc8c3c48deb5007ab2e4675e5922ac1cb76
Superr4y <gitlab@lowsec.net>
2018-06-13 13:14:42 +0200

    add jupyter to container

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ MAINTAINER superr4y
 
 RUN apt-get update -y && apt-get upgrade -y
 RUN apt-get install -y ranger python3 ipython3 python3-matplotlib python3-pandas python3-sklearn
-RUN apt-get install python3-pip 
-RUN pip3 install statsmodels
+RUN apt-get install -y python3-pip mlocate
+RUN pip3 install statsmodels jupyter
 
+EXPOSE 8888
 VOLUME /share
--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 build=${1:-'n'}
 if [ "$build" = 'y' ]; then
     sudo docker build -t superr4y/ml .
+elif [ "$build" = 'inside' ]; then
+    cd /share && jupyter notebook --allow-root --ip="*" --no-browser
 else
-    sudo docker run -it --rm -v "$(pwd)":/share superr4y/ml /bin/bash
+    sudo docker run -p 8888:8888 -it --rm -v "$(pwd)":/share superr4y/ml /share/run.sh inside
 fi
